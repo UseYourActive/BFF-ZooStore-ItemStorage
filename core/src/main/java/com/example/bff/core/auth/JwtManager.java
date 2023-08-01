235f327860795f07ddc676223720479f7f232857
Alex Orozov <alexorozov@gmail.com>
2023-08-01 12:00:35 +0300

    Update on the Spring Security and added swagger configuration.

--- core/src/main/java/com/example/bff/core/auth/JwtManager.java
+++ core/src/main/java/com/example/bff/core/auth/JwtManager.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.example.bff.api.operations.auth.login.LoginUserRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class JwtManager {
     private final Duration TOKEN_VALIDITY = Duration.of(30, ChronoUnit.DAYS);
     private final ApplicationUserDetailsService applicationUserDetailsService;
 
-    //@Value("${jwt-secret}")
+    @Value("${jwt-secret}")
     private String jwtSecret;
 
     public String generateJwt(LoginUserRequest request) {
---
+++ rest/src/main/java/com/example/bff/rest/security/SwaggerConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.bff.rest.security;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer")
+@Configuration
+public class SwaggerConfiguration {
+}
